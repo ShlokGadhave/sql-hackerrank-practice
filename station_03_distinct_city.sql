28ddaf4a5f50219c65d5c441fe1d7cb1c9514ab6
Shlok Gadhave <shlokgadhave20@gmail.com>
2026-02-07 20:11:23 +0530

    Add files via upload

---
+++ station_03_distinct_city.sql
@@ -0,0 +1,8 @@
+Platform-HackerRank
+Problem-Weather Observation Station 3 
+Description-Retrieve distinct city names from the STATION table and sort them alphabetically.
+Concepts- SELECT, DISTINCT, ORDER BY
+Why add-Shows basic data filtering and sorting.
+
+Query:
+SELECT CITY,STATE FROM STATION;
